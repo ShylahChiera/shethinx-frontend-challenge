4d5246f19d24746e8ea69846fdb35ef30bf9a3e3
Shylah Chiera <shylah.chiera@gmail.com>
2018-11-17 10:11:29 -0500

    product template

---
+++ src/scripts/productinfo.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var products = [
+    {
+      type: "high-waist",
+      price: 35.00,
+      subtitle: "Heavy days. Holds up to 2 tampons worth.",
+      desc: "test",
+      image: "test"
+    }
+];
